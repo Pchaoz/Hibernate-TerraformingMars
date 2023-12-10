07ff7385d408a44ffea0bac39b34f9da8246a5eb
raulmartincaro <acastetp@ies-sabadell.cat>
2023-12-10 19:42:14 +0100

    UpdateGames

--- TerraformingMars/src/main/java/main/MainAlex.java
+++ TerraformingMars/src/main/java/main/MainAlex.java
@@ -1,8 +1,12 @@
 package main;
 
 import DAO.DAOCorporation;
+import DAO.DAOGames;
 import DAO.DAOPlayer;
 import DAO.Utils;
+import Models.Corporations;
+import Models.Games;
+import Models.Players;
 
 public class MainAlex {
 
@@ -10,8 +14,22 @@ public class MainAlex {
 		// TODO Auto-generated method stub
 		DAOPlayer daop = new DAOPlayer();
 		DAOCorporation daoC = new DAOCorporation();
+		DAOGames daoG = new DAOGames();
 		daoC.StartCorporations();
 		daop.GeneratePlayers();
+		SetWinner(daop, daoC, daoG);
+	}
+	private static void SetWinner(DAOPlayer DAOp, DAOCorporation DAOc, DAOGames DAOg) {
+		Corporations Guanyador = new Corporations();
+		for (Corporations c : DAOc.Llistar()) {
+			if(c.getVictorypoints()>Guanyador.getVictorypoints()) {
+				Guanyador=c;
+			}
+		}
+		Players Winner = Guanyador.getPlayer();
+		Games g =DAOg.Search(1);
+		g.setGuanyador(Winner);
+	
 	}
 
 }
