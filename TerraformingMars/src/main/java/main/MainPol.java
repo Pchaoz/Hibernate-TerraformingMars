31e32da0f2e797c1b3da3e984c3db79db9b69c70
Pchaozz <polsh2001.1@gmail.com>
2023-12-11 09:38:46 +0100

    Create MainPol.java

---
+++ TerraformingMars/src/main/java/main/MainPol.java
@@ -0,0 +1,12 @@
+package main;
+import Models.*;
+
+public class MainPol {
+	
+	
+
+	public static void main(String[] args) {
+		
+		Corporations credicor = new Corporations("Credicor", "Base game coorporation", 0, null);
+	}
+}
