fe61b41b305972fe49f5d2e15b2e7a15545e2baf
jfuentesp <josefupe91@gmail.com>
2023-12-11 04:03:34 +0100

    TirarDados done

--- TerraformingMars/src/main/java/DAO/DAOPlayer.java
+++ TerraformingMars/src/main/java/DAO/DAOPlayer.java
@@ -3,11 +3,12 @@ package DAO;
 import Models.Players;
 
 public class DAOPlayer extends DAOGeneric<Players, Integer>{
-
+	
 	public DAOPlayer() {
 		super(Players.class);
 		// TODO Auto-generated constructor stub
 	}
+	
 	public void GeneratePlayer(String name) {
 		Players p = new Players(name);
 		this.Persistir(p);
--- TerraformingMars/src/main/java/main/MainJose.java
+++ TerraformingMars/src/main/java/main/MainJose.java
@@ -1,15 +1,46 @@
 package main;
 
+import java.util.Random;
+
 import DAO.DAOMakers;
+import DAO.DAOPlayer;
+import Models.Corporations;
+import Models.Players;
 
 public class MainJose {
+	
+	private static Random rand = new Random();
 	
 	public static void main(String[] args) {
 
 		DAOMakers daoMakers = new DAOMakers();
+		DAOPlayer daoPlayers = new DAOPlayer();
+		daoPlayers.GeneratePlayer("Jose");
+		Players player = daoPlayers.Search(1);
 		
 		daoMakers.generarTablero();
+		RollDices(player);
+		RollDices(player);
+		RollDices(player);
 
+	}
+	
+	public static void RollDices(Players player) 
+	{
+		int [] dados = new int [6];
+		System.out.print("El jugador " + player.getName() + " ha sacado los resultados: ");
+		for (int i : dados) {
+			int tirada = rand.nextInt(6)+1;
+			dados[i] = tirada;
+			System.out.print(tirada +", ");
+		}
+		
+		System.out.println();
+	}
+	
+	public static void ResolveDices(Corporations playerCorporation, int[] dices)
+	{
+		
 	}
 
 }
